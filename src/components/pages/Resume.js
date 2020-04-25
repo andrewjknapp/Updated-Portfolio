466d1ff437816b9f5d90af468d5e9b503cabb3ee
Armadillo <ajknapp2015@gmail.com>
2020-04-24 23:09:56 -0700

    Added iframe as pdf viewer for resume and styling to fit page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../assets/css/Resume.css";
 
 function Resume() {
-    return (<div className="Resume"></div>)
+    return (<iframe src={require("../assets/Andrew_Resume.pdf")} title="resume" className="Resume"></iframe>)
 }
 
 export default Resume;
